db565530394dec31ae304b2022522fec6fad0160
Remy-P78 <remy80.dev@gmail.com>
2023-09-14 14:18:03 +0200

    pres push

---
+++ src/app/models/users.ts
@@ -0,0 +1,12 @@
+export interface Users {
+    id_user: number;
+    username: string;
+    firstname: string;
+    description: string;
+    email: string;
+    city: string;
+    password: string;
+    departement: number;
+    // id_photo: number;
+    // id_gender_user: number;
+}
