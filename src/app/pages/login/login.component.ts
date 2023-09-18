3ff5cc9fdde6fb4b6f727285f6651d32c2d2b5b4
LOX91 <109281442+LOX91@users.noreply.github.com>
2023-09-18 16:06:01 +0200

    Recupe de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  bioSection = this.fb.group({
+    userName: ['', Validators.required],
+    passWord: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {}
+  callingFunction() {
+    console.log(this.bioSection.value);
+  }
 }
