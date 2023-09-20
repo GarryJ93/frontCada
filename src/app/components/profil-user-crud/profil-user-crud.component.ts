1f7508a633cbfc9e8d8b8b463e8b8db60855bc25
macsampro <112323681+macsampro@users.noreply.github.com>
2023-09-20 14:52:47 +0200

    V 1.0 profil user

--- src/app/components/profil-user-crud/profil-user-crud.component.ts
+++ src/app/components/profil-user-crud/profil-user-crud.component.ts
@@ -1,19 +1,52 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Users } from 'src/app/models/users';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-profil-user-crud',
   templateUrl: './profil-user-crud.component.html',
   styleUrls: ['./profil-user-crud.component.css']
 })
-export class ProfilUserCrudComponent {
-  FormProfilUser!:FormGroup;
+export class ProfilUserCrudComponent implements OnInit {
+  formProfilUser!: FormGroup;
   profilUser!: Users;
 
-
   constructor(
-    private
-  )
+    private userService: UsersService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.formProfilUser = this.formBuilder.group({
+      firstname: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      departement: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    let profilUser: Users = { ...this.formProfilUser.value };
+    if (!this.formProfilUser.valid) {
+      console.log("Le formulaire n'est pas valide");
+      return;
+    }
+    profilUser.id_gender_user = 2; // À adapter en fonction de votre logique
+    this.userService.addUser(profilUser).subscribe({
+      next: () => {
+        alert('Utilisateur ajouté avec succès !');
+        this.formProfilUser.reset();
+        this.router.navigate(['/login']);
+      },
+      error: (error) => {
+        console.error("Erreur lors de l'ajout de l'utilisateur", error);
+      }
+    });
+  }
 }
