c5dba0d108171e6345a57a3c6b84675a3e4ce82c
Remy-P78 <remy80.dev@gmail.com>
2023-09-29 11:00:35 +0200

    filtres et currentUser dans profil ok en principe

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -14,7 +14,6 @@ import { UsersService } from 'src/app/services/users.service';
   styleUrls: ['./accueil.component.css'],
 })
 export class AccueilComponent {
-  currentUser!: Users;
   userToDisplay!: Users[];
   allUsers: Users[] = [];
   animalsToDisplay!: Animals[];
@@ -23,29 +22,33 @@ export class AccueilComponent {
   genderUsers!: GenderUser[];
   categoriesFiltered: string[] = [];
   departementFiltered!: number;
-  genderFiltered!: string;
+  genderFiltered: string = 'Genre';
   photosToDisplay!: Photos[];
   allPhotos: Photos[] = [];
   isImageLoading!: Boolean;
   imageToShow!: any;
-  
+
   constructor(
     private usersService: UsersService,
-    
+
     private photoService: PhotosService,
-    
-    private animalsService: AnimalsService,
-    
+
+    // private animalsService: AnimalsService,
+
     private genderService: GenderUserService
-    ) {}
-    
-    currentUserId = this.usersService.getUserConnected()!;
-    ngOnInit() {
-      this.usersService.getAllUsers().subscribe({
+  ) {}
+
+  currentUserId = this.usersService.getUserConnected()!;
+  ngOnInit() {
+    this.usersService.getAllUsers().subscribe({
       next: (response) => {
         {
-          this.allUsers = [...response];
-          this.userToDisplay = [...response];
+          this.allUsers = [...response].filter(
+            (user) => user.id_user !== this.currentUserId
+          );
+          this.userToDisplay = [...response].filter(
+            (user) => user.id_user !== this.currentUserId
+          );
           this.userDepartement = [
             ...new Set(this.userToDisplay.map((user) => user.departement)),
           ];
@@ -53,80 +56,72 @@ export class AccueilComponent {
       },
     });
 
-    this.usersService.getUserById(this.currentUserId).subscribe({
-      next: (response) => {
-        this.currentUser = response;
-      },
-    });
-    
-    
-    this.animalsService.getAllAnimals().subscribe({
-      next: (response) => {
-        {
-          this.allAnimals = [...response];
-          this.animalsToDisplay = [...response];
-        }
-      },
-    });
-    
+    // this.animalsService.getAllAnimals().subscribe({
+    //   next: (response) => {
+    //     {
+    //       this.allAnimals = [...response];
+    //       this.animalsToDisplay = [...response];
+    //     }
+    //   },
+    // });
+
     this.genderService.getGenderUsers().subscribe({
       next: (response) => {
         this.genderUsers = [...response];
       },
     });
   }
-  
+
   categoriesReceived(categoriesSelected: string[]) {
     this.categoriesFiltered = categoriesSelected;
     this.letsFilter();
   }
-  
+
   departementReceived(departementSelected: number) {
     this.departementFiltered = departementSelected;
     this.letsFilter();
   }
-  
+
   genderReceived(genderSelected: string) {
     this.genderFiltered = genderSelected;
     this.letsFilter();
   }
-  
+
   letsFilter() {
     this.userToDisplay = [...this.allUsers];
-    
+
     if (this.categoriesFiltered.length != 0) {
       this.userToDisplay = this.userToDisplay.filter((user) =>
-      user.animal.some((animal) =>
-      this.categoriesFiltered.includes(animal.breed.species.species)
-      )
+        user.animal.some((animal) =>
+          this.categoriesFiltered.includes(animal.breed.species.species)
+        )
       );
     }
-    
+
     if (this.departementFiltered) {
       this.userToDisplay = this.userToDisplay.filter(
         (user) => user.departement === this.departementFiltered
-        );
-      }
+      );
+    }
 
     if (this.genderFiltered != 'Genre') {
       this.userToDisplay = this.userToDisplay.filter(
         (user) => user.gender_user.gender === this.genderFiltered
       );
     }
-    
+
     //Si tous les filtres sont remis à valeur initiale
     if (
       this.categoriesFiltered.length === 0 &&
       this.departementFiltered === 0 &&
       this.genderFiltered == 'Genre'
-      ) {
-        this.userToDisplay = [...this.allUsers];
-        // console.log('courante', this.currentUser);
-      }
+    ) {
+      this.userToDisplay = [...this.allUsers];
     }
-    
-    async createImageFromBlob(image: Blob) {
-      let reader = await new FileReader();
+  }
+
+  async createImageFromBlob(image: Blob) {
+    let reader = await new FileReader();
     reader.readAsDataURL(image);
     reader.addEventListener('load', () => {
       this.imageToShow = reader.result;
