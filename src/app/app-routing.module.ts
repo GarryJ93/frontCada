b20f6e256dc33d55c4d4b0d2307f6f329136025f
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-09-29 12:18:42 +0200

    creation guard pour user connecté

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,17 @@ import { LoginComponent } from './pages/login/login.component';
 import { MessageComponent } from './pages/message/message.component';
 import { ConsultationComponent } from './pages/consultation/consultation.component';
 import { authGuard } from './auth.guard';
+import { connectedGuard } from './connected.guard';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: "full"},
+  { path: '', redirectTo: 'home', pathMatch: "full" },
   {path: 'accueil', component: AccueilComponent, canActivate:[authGuard] },
   { path: 'accueil/:id', component: AccueilComponent, canActivate: [authGuard] },
-  {path: 'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent, canActivate:[connectedGuard]},
   { path: 'message', component: MessageComponent, canActivate: [authGuard] },
   { path: 'profil', component: ProfileComponent, canActivate: [authGuard] },
-  {path: 'signin', component: SigninComponent},
-  {path: 'login', component: LoginComponent },
+  { path: 'signin', component: SigninComponent, canActivate: [connectedGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [connectedGuard] },
   { path: 'consultation/:id', component: ConsultationComponent, canActivate: [authGuard] },
   {path: '**', component: Page404Component}];
 
